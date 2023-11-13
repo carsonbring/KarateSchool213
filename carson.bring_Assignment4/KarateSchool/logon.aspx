26c6743c7305c9c0b548bc60f2279b6a4ffdfb2d
Carson Bring <79489533+carsonbring@users.noreply.github.com>
2023-11-12 19:00:02 -0600

    Logon

---
+++ carson.bring_Assignment4/KarateSchool/logon.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="logon.aspx.cs" Inherits="KarateSchool.logon" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Login ID="Login1" runat="server" BackColor="#F7F7DE" BorderColor="#CCCC99" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="10pt" Height="139px" OnAuthenticate="Login1_Authenticate" Width="247px">
+        <TitleTextStyle BackColor="#6B696B" Font-Bold="True" ForeColor="#FFFFFF" />
+    </asp:Login>
+</asp:Content>
---
+++ carson.bring_Assignment4/KarateSchool/logon.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KarateSchool
+{
+    public partial class logon : System.Web.UI.Page
+    {
+        string connString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\carso\\OneDrive\\Desktop\\KarateSchool213\\carson.bring_Assignment4\\KarateSchool\\App_Data\\KarateSchool.mdf;Integrated Security=True;Connect Timeout=30";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            string userName = Login1.UserName;
+            string password = Login1.Password;
+
+            using (KarateSchoolDataContext context = new KarateSchoolDataContext(connString))
+            {
+                var user = context.NetUsers.SingleOrDefault(u => u.UserName == userName && u.UserPassword == password);
+
+                if (user != null)
+                {
+                    // Credentials are correct
+                    FormsAuthentication.RedirectFromLoginPage(userName, true);
+                }
+                else
+                {
+                    // Credentials are incorrect
+                    Response.Redirect("logon.aspx", true);
+                }
+            }
+        }
+    }
+}
