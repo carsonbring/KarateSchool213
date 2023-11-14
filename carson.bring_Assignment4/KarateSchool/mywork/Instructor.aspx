d8ff133038c01c4666dbd749002e8eeb37e77079
Carson Bring <79489533+carsonbring@users.noreply.github.com>
2023-11-14 11:32:08 -0600

    Instructor page logic

--- carson.bring_Assignment4/KarateSchool/mywork/Instructor.aspx
+++ carson.bring_Assignment4/KarateSchool/mywork/Instructor.aspx
@@ -4,6 +4,18 @@
     <p>Hello,
         <asp:Label ID="Name" runat="server" Text="Label"></asp:Label>
     </p>
-    <p>&nbsp;</p>
+    <p>
+        <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="White" BorderStyle="Ridge" BorderWidth="2px" CellPadding="3" CellSpacing="1" GridLines="None">
+            <FooterStyle BackColor="#C6C3C6" ForeColor="Black" />
+            <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#E7E7FF" />
+            <PagerStyle BackColor="#C6C3C6" ForeColor="Black" HorizontalAlign="Right" />
+            <RowStyle BackColor="#DEDFDE" ForeColor="Black" />
+            <SelectedRowStyle BackColor="#9471DE" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#594B9C" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#33276A" />
+        </asp:GridView>
+    </p>
     <p>&nbsp;</p>
 </asp:Content>
--- carson.bring_Assignment4/KarateSchool/mywork/Instructor.aspx.cs
+++ carson.bring_Assignment4/KarateSchool/mywork/Instructor.aspx.cs
@@ -9,11 +9,35 @@ namespace KarateSchool.mywork
 {
     public partial class Instructor : System.Web.UI.Page
     {
+        string connString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\carso\\OneDrive\\Desktop\\KarateSchool213\\carson.bring_Assignment4\\KarateSchool\\App_Data\\KarateSchool.mdf;Integrated Security=True;Connect Timeout=30";
+        KarateSchoolDataContext context;
         protected void Page_Load(object sender, EventArgs e)
         {
-            string firstname = Session["FirstName"] as string;
-            string lastname = Session["LastName"] as string;
-            Name.Text = firstname + " " + lastname;
+            if (!IsPostBack)
+            {
+
+                context = new KarateSchoolDataContext(connString);
+                string firstname = Session["FirstName"] as string;
+                string lastname = Session["LastName"] as string;
+                int userid = Convert.ToInt32(Session["UserID"]);
+
+                Name.Text = firstname + " " + lastname;
+
+                var query = from section in context.Sections
+                            join member in context.Members
+                            on section.Member_ID equals member.Member_UserID
+                            where section.Instructor_ID == userid
+                            select new
+                            {
+                                section.SectionName,
+                                MemberName = member.MemberFirstName + " " + member.MemberLastName
+                                
+                            };
+                
+                
+                GridView1.DataSource = query.ToList();
+                GridView1.DataBind();
+            }
         }
     }
 }
